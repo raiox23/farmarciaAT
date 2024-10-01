f62072b044fc9a7f4ea94bb953866ba7aa934070
RYAN COSTA KRUCH <ryan_kruch@estudante.sesisenai.org.br>
2024-10-01 16:32:53 -0300

    at farmarcia

---
+++ atFarmarcia/src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./header.css"
+import Logo from './Logo'
+
+
+function Header() {
+  return (
+    <div className='headerConteiner'>
+
+      <div className='divImg'>
+        <Logo />
+
+      </div>
+
+
+    </div>
+  )
+}
+
+export default Header
